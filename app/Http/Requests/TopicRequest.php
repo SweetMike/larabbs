9d869855ba323fb529b5cd821383c92715e4a1c0
mike.shen <mike.shen@atommatrix.com>
2018-04-13 09:31:25 +0800

    Prevent XSS injection

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -34,6 +34,7 @@ class TopicRequest extends Request
         return [
             'title.min' => '标题必须至少两个字符',
             'body.min' => '文章内容必须至少三个字符',
+            'body.required' => '文章内容不能为空',
         ];
     }
 }
