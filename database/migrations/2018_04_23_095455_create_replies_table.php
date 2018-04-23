f0c229d0a0189a80a35a7f640d4944a0d77da26c
mike.shen <mike.shen@atommatrix.com>
2018-04-23 14:27:15 +0800

    List user replies

---
+++ database/migrations/2018_04_23_095455_create_replies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('topic_id')->unsigned()->index()->default(0);
+            $table->integer('user_id')->unsigned()->index()->default(0);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('replies', function (Blueprint $table) {
+            Schema::drop('replies');
+        });
+    }
+}
